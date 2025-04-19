0e0dd22aebda35c5c96cd932f9d1370875f797d9
Alif Pahreza <agzmv123@gmail.com>
2025-04-20 05:22:58 +0800

    fix: update reset password redirect URL to use custom scheme

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,8 @@ Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
 
 Route::post('/forgot-password', [AuthController::class, 'sendResetPassword']);
 
-Route::get('/reset-password/{token}', function (Request $request, $token){
-    return redirect()->away(config('app.frontend_url') . '/reset-password?token=' . $token . '&email=' . urlencode($request->email));
+Route::get('/reset-password/{token}', function (Request $request, $token) {
+    return redirect()->away("tumbuh://resetPassword?token={$token}&email=" . urlencode($request->email));
 })->name('password.reset');
 
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
@@ -33,7 +33,7 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::get('/test-email', function () {
     Mail::raw('Halo dari TUMBUH!', function ($message) {
         $message->to('adef6477@gmail.com')
-                ->subject('Tes Email dari Resend');
+            ->subject('Tes Email dari Resend');
     });
 
     return 'Email terkirim';
