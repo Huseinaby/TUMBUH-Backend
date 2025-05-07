5c2f67af5505eb5111ee9fa0715cd5afdc606bac
Abu Husein <huseinabu01@gmail.com>
2025-05-07 15:36:09 +0800

    refactor logout method to improve token deletion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -90,12 +90,17 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $token = $request->user()->currentAccessToken();
+
+        if($token) {
+            $token->delete();
+        }
 
         return response()->json([
             'message' => 'User logged out successfully'
         ], 200);
     }
+
     public function handleProviderCallback(Request $request)
     {
         try {
