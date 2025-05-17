6d0afeeeffe79af8628b5bc359acec486a63bcfd
Abu Husein <huseinabu01@gmail.com>
2025-05-17 11:44:16 +0800

    refactor: Revert changes to 'web.php' route name variable

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    $name = 'test actions';
-    return $name;
+    return view('welcome');
+    // $name = 'test actions';
+    // return $name;
 });
