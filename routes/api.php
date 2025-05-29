bd29c1b21d84889d4e5db6e940a4363275549072
Abu Husein <huseinabu01@gmail.com>
2025-05-29 16:10:03 +0800

    fix: Correct typo in route definition for deleting product images

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::post('/product', [productController::class, 'store']);
 Route::put('/product/{id}', [productController::class, 'update']);
 Route::delete('/product/{id}', [productController::class, 'destroy']);
 Route::get('/product/user/{userId}', [productController::class, 'getProductByUser']);   
-Route::detele('/product/image/{id}', [productController::class, 'destroyImage']);
+Route::delete('/product/image/{id}', [productController::class, 'destroyImage']);
 
 //cartItem
 Route::get('/cart', [cartController::class, 'index']);
