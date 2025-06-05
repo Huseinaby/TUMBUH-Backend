65965beeb43213ff6d3b55ae6c8f223fc79c3ec3
Abu Husein <huseinabu01@gmail.com>
2025-06-05 09:33:39 +0800

    fix: correct class name casing in orderItems relationship method

--- app/Models/transaction.php
+++ app/Models/transaction.php
@@ -22,7 +22,7 @@ class transaction extends Model
 
     public function orderItems()
     {
-        return $this->hasMany(OrderItem::class);
+        return $this->hasMany(orderItem::class);
     }
 
     public function seller()
