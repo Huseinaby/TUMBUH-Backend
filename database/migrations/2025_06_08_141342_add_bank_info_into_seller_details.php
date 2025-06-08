c5c1f5a357f23e832e2b1f3d9bac2562fcdea383
Abu Husein <huseinabu01@gmail.com>
2025-06-08 14:15:15 +0800

    feat: remove platform_fee migration and add bank info fields to seller_details

---
+++ database/migrations/2025_06_08_141342_add_bank_info_into_seller_details.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('seller_details', function (Blueprint $table) {
+            $table->string('bank_name')->nullable()->after('store_banner');
+            $table->string('bank_account_number')->nullable()->after('bank_name');
+            $table->string('bank_account_holder_name')->nullable()->after('bank_account_number');
+            $table->string('bank_ifsc_code')->nullable()->after('bank_account_holder_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
