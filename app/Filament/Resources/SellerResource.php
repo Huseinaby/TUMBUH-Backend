0d06343ff135eeda9a46d8e828eff6c2fa9cca67
Abu Husein <huseinabu01@gmail.com>
2025-06-09 12:19:27 +0800

    feat: implement seller resource management with create, edit, and list functionalities

---
+++ app/Filament/Resources/SellerResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SellerResource\Pages;
+use App\Filament\Resources\SellerResource\RelationManagers;
+use App\Models\SellerDetail;
+use Dom\Text;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SellerResource extends Resource
+{
+    protected static ?string $model = SellerDetail::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-storefront';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('user.username')
+                    ->label('Username')
+                    ->searchable(),
+                TextColumn::make('store_name')
+                    ->label('Store Name')
+                    ->searchable(),
+                ImageColumn::make('store_logo')
+                    ->label('Store Logo')
+                    ->circular(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSellers::route('/'),
+            'create' => Pages\CreateSeller::route('/create'),
+            'edit' => Pages\EditSeller::route('/{record}/edit'),
+        ];
+    }
+}
