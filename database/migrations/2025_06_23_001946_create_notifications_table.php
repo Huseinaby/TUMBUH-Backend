bffed0242ae8622256bf5d97ff8c659fd1af564a
Abu Husein <huseinabu01@gmail.com>
2025-06-23 00:38:28 +0800

    fix notifications migrations

--- database/migrations/2025_06_23_001946_create_notifications_table.php
+++ database/migrations/2025_06_23_001946_create_notifications_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('message');
-            $table->string('type')->default('info'); // info, success, warning, error
+            $table->string('type')->default('info'); 
             $table->boolean('read')->default(false);
             $table->timestamps();
         });
