29f1a331624a9a9061f6bd1e191645073d533be3
Abu Husein <huseinabu01@gmail.com>
2025-06-23 19:16:10 +0800

    del

--- resources/views/testPusher.blade.php
+++ resources/views/testPusher.blade.php
@@ -13,7 +13,7 @@
     // Inisialisasi Pusher
     const pusher = new Pusher('ba8f0597dd4a00b837c1', {
       cluster: 'ap1',
-      authEndpoint: 'https://tumbuh.vps-poliban.my.id/api/broadcasting/auth',
+      authEndpoint: 'https://tumbuh.vps-poliban.my.id/broadcasting/auth',
       auth: {
         headers: {
           Authorization: 'Bearer 15|5FlmLzyU5QZxD68lYK52axSBUmktcoMTiZeGnh817d82e74d',
