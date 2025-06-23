a0dcf440036e95607f9e396cf8ccddc7b231a61f
Abu Husein <huseinabu01@gmail.com>
2025-06-23 19:20:14 +0800

    feat: add authentication middleware for broadcast routes

--- routes/api.php
+++ routes/api.php
@@ -164,6 +164,8 @@ Route::put('/notification/{id}/read', [notificationController::class, 'markAsRea
 //test generate quiz
 Route::post('/quiz/generate', [QuizController::class, 'generateQuiz']);
 
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 
 //Test Email
 Route::get('/test-email', function () {
