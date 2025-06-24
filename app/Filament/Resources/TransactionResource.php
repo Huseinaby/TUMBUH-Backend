30f22d4b0c2cef717531822b9f5fe32c2ac6c181
Abu Husein <huseinabu01@gmail.com>
2025-06-24 14:14:21 +0800

    feat: add Transaction resource with CRUD pages for managing transactions

---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\DeleteAction;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+    protected static ?string $label = 'Transaction';
+    protected static ?string $navigationGroup = 'Marketplace';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('user.username')->label('user'),
+                TextColumn::make('status')->badge(),
+                TextColumn::make('total_price')->money('IDR'),
+                TextColumn::make('created_at')->label('tanggal')->dateTime()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }
+}
