177b5e53f867c0810f4dd6bdda7169b68a64bc8f
Abu Husein <huseinabu01@gmail.com>
2025-07-02 16:53:03 +0800

    fix: update image path handling in CartItemResource for consistency

--- app/Http/Resources/CartItemResource.php
+++ app/Http/Resources/CartItemResource.php
@@ -25,7 +25,7 @@ class CartItemResource extends JsonResource
                 'price' => $this->product->price,
                 'stock' => $this->product->stock,
                 'image' => $this->product->images->first()
-                    ? asset('storage/' . $this->product->images->first()->image_path)
+                    ? asset($this->product->images->first()->image_path)
                     : null,
             ]
 
