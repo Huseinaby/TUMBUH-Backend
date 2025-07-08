ba5074ecae4a22813429f184cfff3829e5b3f87d
Abu Husein <huseinabu01@gmail.com>
2025-07-08 21:09:33 +0800

    refactor: remove unused getImagePathAttribute method from ProductImage model

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,11 +8,6 @@ class ProductImage extends Model
 {
     protected $guarded = ['id'];
 
-    public function getImagePathAttribute($value)
-    {
-        return 'storage/' . $value;
-    }
-
     public function product()
     {
         return $this->belongsTo(Product::class);
