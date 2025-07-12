ce197046dc41266af53f53dd534de4f1da3ae774
Abu Husein <huseinabu01@gmail.com>
2025-07-13 06:23:34 +0800

    test back to transaction

--- app/Http/Controllers/transactionController.php
+++ app/Http/Controllers/transactionController.php
@@ -63,7 +63,7 @@ class transactionController extends Controller
         }
 
         return response()->json([
-            'transactions' => TransactionResource::collection($transactions)->resolve(),
+            'transactions' => $transactions,
         ]);
     }
 
