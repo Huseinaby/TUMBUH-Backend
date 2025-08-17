ac0215623f16832040ab3cded926b6fa4dd8505f
Abu Husein <huseinabu01@gmail.com>
2025-08-18 01:00:48 +0800

    add group model back

---
+++ app/Models/Group.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    protected $fillable = [
+        'province_id',
+        'city_id',
+        'name',
+        'slug',
+        'description',
+        'city',
+        'cover_image',
+        'created_by',
+    ];
+    
+
+    public function members()
+    {
+        return $this->hasMany(GroupMember::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+}
