d69ac7f8542a09a5ccf82122d3a099c39ccd7089
Huseinaby <huseinabu01@gmail.com>
2025-09-27 21:00:13 +0800

    feat: add Device model and migration for devices table

---
+++ app/Models/Device.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'serial_number',
+        'device_name',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
